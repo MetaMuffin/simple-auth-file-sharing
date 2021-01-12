71b7bb133074a3d8b09d98ed42a01bbe9c288074
MetaMuffin <metamuffin@yandex.com>
2021-01-12 15:47:32 +0100

    added cgi support

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 
-import auth from "basic-auth";
 import express from "express"
 import { join } from "path"
 const app = express()
-import { stat, readdir } from "fs/promises"
+import { stat, readdir, access } from "fs/promises"
 import http from "http"
 import https from "https"
 
-import { Response } from "express"
+import { Response, Request } from "express"
 import { existsSync, readFileSync } from "fs";
+import { exec } from "child_process";
 
 app.set('view engine', 'ejs');
 
@@ -21,14 +21,27 @@ function safePathName(s: string): string {
     return s.replace(/[\.\/]/, "")
 }
 
-function sanitizeFn(s:string): string {
-    return s.replace("..","").replace(/\/.$/g,"").replace("//","").replace(/[0x00–0x1f0x80–0x9f\/\\\|\:\<\>\"\?\*]/g,"")
+function sanitizeFn(s: string): string {
+    return s.replace("..", "").replace(/\/.$/g, "").replace("//", "").replace(/[0x00–0x1f0x80–0x9f\/\\\|\:\<\>\"\?\*]/g, "")
+}
+
+function useCGI(req: Request, res: Response, fpath: string) {
+    exec(fpath, (err, stdout, stderr) => {
+        if (err) return res.status(500).send(`Error: ${stderr}`)
+        res.status(200).send(stdout)
+    })
+}
+
+function authRefuse(res: Response) {
+    res.writeHead(401, { 'WWW-Authenticate': 'Basic realm="incorrect credencials"' });
+    return res.end("Incorrect credentials")
 }
 
 app.use(async (req, res, next) => {
-    const credentials = auth(req);
-    if (credentials) {
-        var fname = safePathName(credentials.name) + ":" + safePathName(credentials.pass)
+    if (req.headers.authorization) {
+        var userpass = Buffer.from((req.headers.authorization).split(' ')[1] || '', 'base64').toString();
+
+        var fname = safePathName(userpass)
         var spath = sanitizeFn(req.url)
         var fpath = join(__dirname, "files", fname, spath).replace(/\/$/g, "")
         console.log(`[${req.ip}] ${fpath}`);
@@ -36,22 +49,37 @@ app.use(async (req, res, next) => {
         try {
             s = await stat(fpath)
         } catch (e) {
-            return res.status(401).send("Halt Stop!")
+            return authRefuse(res)
         }
+
         if (s.isDirectory()) {
-            var files = await readdir(fpath)
-            return res.render("dirlist", { files })
+            var files = await Promise.all((await readdir(fpath)).map(async (fn) => {
+                var sta = await stat(join(fpath, fn))
+                var dir = sta.isDirectory()
+                var cgi = !!(sta.mode & 0o0111) && !dir
+                return { name: fn, cgi, dir }
+            }))
+            return res.render("dirlist", { files, dirname: "/" + spath })
+        }
+
+        if (s.mode & 0o0111) {
+            return useCGI(req, res, fpath)
         }
         if (s.isFile()) {
             return res.sendFile(fpath)
         }
         res.status(500).send("Content not availible.")
     }
-    res.set(`WWW-Authenticate', 'Basic realm="${Math.random() * 10000}"`); // TODO use secure randomness
-    return res.status(401).send("Halt Stop!");
+    return authRefuse(res)
 });
+app.listen(8089, "0.0.0.0", () => {
+    console.log("listening http service on :::8089");
+})
+// const srv = http.createServer(app)
+// srv.listen(8089, "0.0.0.0", () => {
+//     console.log("listening http service on :::8089");
+// })
 
-const srv = http.createServer({}, app)
 if (existsSync(join(__dirname, "../certs"))) {
     const srvs = https.createServer({
         cert: readFileSync(join(__dirname, "../certs/cert.pem")),
@@ -61,7 +89,4 @@ if (existsSync(join(__dirname, "../certs"))) {
         console.log("listening https service on :::8088");
     })
 }
-srv.listen(8089, "0.0.0.0", () => {
-    console.log("listening http service on :::8089");
-})
 
